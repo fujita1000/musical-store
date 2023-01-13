68f6292001c8d1b6a53b8fc0b25b9fbb5c9a9f3d
{fujitanao} <fujitanao.rmt@gmail.com>
2023-01-13 18:56:36 +0900

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 
 function MyApp({ Component, pageProps }) {
-  // Use the layout defined at the page level, if available
   return (
     <>
       <PrimaryLayout>
